b6400968c2874fe83c5ca86f0bd9f7858f4b549a
Bliss <pisit.w@23perspective.com>
2020-02-18 14:34:49 +0700

    first commit

---
+++ UIPlayground/NavigationCustomBackButtonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationCustomBackButtonViewController.swift
+//  UIPlayground
+//
+//  Created by Pisit W on 17/2/2563 BE.
+//  Copyright © 2563 23. All rights reserved.
+//
+
+import UIKit
+
+class NavigationCustomBackButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
